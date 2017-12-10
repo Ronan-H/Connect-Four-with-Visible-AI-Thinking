d812b506932ab1ba6d8472dc6b2bd2a33906e3b7
Ronan-H <g00342393@gmit.ie>
2017-12-10 14:40:20 +0000

    Added connect 4 board

--- Connect Four with Visible AI Thinking/MainPage.xaml.cs
+++ Connect Four with Visible AI Thinking/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,11 +24,20 @@ namespace Connect_Four_with_Visible_AI_Thinking
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        /* Represents the board state (state of the chips)
+         * 0 -> blank (no chip)
+         * 1 -> red chip
+         * 2 -> yellow chip
+         */
+        int[,] _board = new int[6, 7];
+        Ellipse[,] _chips = new Ellipse[6, 7];
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        Boolean setupDone = false;
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
@@ -36,8 +47,56 @@ namespace Connect_Four_with_Visible_AI_Thinking
             // scale the board correctly
             Loaded += delegate
             {
-                boardGrid.Width = (rootPanel.ActualHeight / 6) * 7;
-                settingsPanel.Width = rootPanel.ActualWidth - boardGrid.Width;
+                if (!setupDone)
+                {
+                    boardGrid.Width = (rootPanel.ActualHeight / 6) * 7;
+                    settingsPanel.Width = rootPanel.ActualWidth - boardGrid.Width;
+
+                    int borderSize = (int)(boardGrid.Width / 7) - 2;
+
+                    for (int i = 0; i < 6; ++i)
+                    {
+                        boardGrid.RowDefinitions.Add(new RowDefinition());
+                    }
+
+                    for (int i = 0; i < 7; ++i)
+                    {
+                        boardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    }
+
+                    // load the chips
+                    for (int i = 0; i < 6; ++i)
+                    {
+                        for (int j = 0; j < 7; ++j)
+                        {
+                            Border border = new Border();
+                            border.Width = borderSize;
+                            border.Height = borderSize;
+                            border.HorizontalAlignment = HorizontalAlignment.Center;
+                            border.VerticalAlignment = VerticalAlignment.Center;
+                            border.Background = new SolidColorBrush(Colors.Blue);
+
+                            Ellipse chip = new Ellipse();
+                            chip.Fill = new SolidColorBrush(Colors.White);
+                            chip.Width = (int)(borderSize * 0.8);
+                            chip.Height = chip.Width;
+
+                            _chips[i, j] = chip;
+                            _board[i, j] = 0;
+
+                            border.SetValue(Grid.RowProperty, i);
+                            border.SetValue(Grid.ColumnProperty, j);
+
+                            chip.SetValue(Grid.RowProperty, i);
+                            chip.SetValue(Grid.ColumnProperty, j);
+
+                            boardGrid.Children.Add(border);
+                            boardGrid.Children.Add(chip);
+                        }
+                    }
+
+                    setupDone = true;
+                }
             };
         }
 
